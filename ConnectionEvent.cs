833c65c5a6102272437fdf531c916ba0a2973ff0
Kelly Elton <kelly.elton@skylabsonline.com>
2011-06-16 09:19:49 +0000

    Added ConnectionEvent.

---
+++ ConnectionEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NetShit
+{
+    public class ConnectionEvent
+    {
+        public eConnectionEvent Event { get; private set; }
+
+        public String Host { get; private set; }
+
+        public int Port { get; private set; }
+
+        public String XData { get; private set; }
+
+        public enum eConnectionEvent { eceConnect, eceDisconnect };
+
+        public ConnectionEvent(String host, int port, eConnectionEvent e)
+        {
+            Host = host;
+            Port = port;
+            Event = e;
+        }
+
+        public ConnectionEvent(String host, int port, String xdata, eConnectionEvent e)
+        {
+            Host = host;
+            Port = port;
+            Event = e;
+            XData = xdata;
+        }
+
+        public static bool operator ==(ConnectionEvent e1, ConnectionEvent e2)
+        {
+            if(e1.Event == e2.Event)
+                return true;
+            return false;
+        }
+    }
+}
